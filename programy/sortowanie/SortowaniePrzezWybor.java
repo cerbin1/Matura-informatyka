799b4d04a8f17f43adfa9a933a32116e2a7de783
cerbin <gekarez@gmail.com>
2018-04-12 15:27:42 +0200

    Dodano klase SortowaniePrzezWybor

---
+++ programy/sortowanie/SortowaniePrzezWybor.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class SortowaniePrzezWybor {
+	public static void main(String... args) {
+		int[] liczby = new int[] { 32, 12345, 2134, 124, 12, 321, 3, 213, 12,
+				321, 31, 4, 1, 5, 2, 64, 3, 634, 643, 6, 2, 1, 3, 5, 4 };
+		
+		for (int i = 0; i < liczby.length; i++) {
+			for (int j = i + 1; j < liczby.length; j++) {
+				if (liczby[j] < liczby[i]) {
+					int temp = liczby[i];
+					liczby[i] = liczby[j];
+					liczby[j] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(liczby));
+	}
+}
