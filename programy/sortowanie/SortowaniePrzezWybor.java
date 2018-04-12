4d0d0d0a0cd2da17dbae0528e9801492b0b51ee1
cerbin <gekarez@gmail.com>
2018-04-12 15:29:56 +0200

    Dodanie linku w komentarzu do wyjasnienia sortowania

--- programy/sortowanie/SortowaniePrzezWybor.java
+++ programy/sortowanie/SortowaniePrzezWybor.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class SortowaniePrzezWybor {
 	public static void main(String... args) {
+        // http://eduinf.waw.pl/inf/alg/003_sort/0009.php
 		int[] liczby = new int[] { 32, 12345, 2134, 124, 12, 321, 3, 213, 12,
 				321, 31, 4, 1, 5, 2, 64, 3, 634, 643, 6, 2, 1, 3, 5, 4 };
 		
