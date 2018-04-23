04f4b05831ddb5ab5c00dbe3859d0e36a1fcc915
cerbin <gekarez@gmail.com>
2018-04-23 14:00:50 +0200

    Dodanie reszty z dzielenia dla BigDecimal

--- programy/DuzeLiczby.java
+++ programy/DuzeLiczby.java
@@ -7,6 +7,11 @@ public class DuzeLiczby {
         System.out.println(bigDecimal.multiply(new BigDecimal(10)));
         System.out.println(bigDecimal.pow(2));
         System.out.println(bigDecimal.subtract(new BigDecimal(5)));
+        System.out.println(bigDecimal.remainder(new BigDecimal(2)));
+
+        if (bigDecimal.remainder(new BigDecimal(3)) == BigDecimal.ZERO) {
+            System.out.println("Liczba " + bigDecimal + " jest podzielna przez trzy.");
+        }
 
         BigInteger bigInteger = new BigInteger("231");
         System.out.println(bigInteger.toString());
